45dacf31ce271668349059a0e8a67495eb57a145
sarah <sarah@DESKTOP-OAUA1DU>
2020-09-18 11:24:20 -0700

    fix links take 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const links = [
     {
       label: "Week 1 - Notes",
-      url: "WDD330-Portfolio/Week01/index.html"
+      url: "Week01/index.html"
     },
     {
         label: "Week 2 - Notes",
-        url: "WDD330-Portfolio/Week02/index.html"
+        url: "Week02/index.html"
       }
   ]
 
